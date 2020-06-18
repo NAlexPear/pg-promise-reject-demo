2f4e03764ad88563cd1d618b2ad4e3573ddb14f6
Alex Pearson <alex@alexpear.com>
2020-06-18 17:15:42 -0400

    🚀 Set up initial failing test case

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Pool } = require('node-postgres')
+
+const pool = new Pool({
+  host: 'postgres',
+  user: 'test',
+  database: 'db',
+  password: 'mysupersecretpassword',
+  port: 5432
+})
+
+const testProcess = async () => {
+  try {
+    const { rows: workingResult } = await pool.query('SELECT 1')
+
+    // this should work
+    console.log('test query', workingResult)
+
+    const { rows: failingResult } = await pool.query('SELECT * FROM notatable')
+
+    // this should never appear under any circumstance
+    console.log('failing query', failingResult)
+  } catch(error) {
+    // this ☝️  be the SQL error ("no relation notatable", etc) from the second query
+    // but that SQL error is never registered as 'caught'
+    // and this error never appears in the console
+    console.error(error)
+  }
+}
+
+testProcess()
