71365720413a14727e2376d153a1e154860d32d1
Alex Pearson <alex@alexpear.com>
2020-06-18 17:47:58 -0400

    🌳 Update logs for clarity

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ const testProcess = async () => {
     const { rows: workingResult } = await pool.query('SELECT 1')
 
     // this should work
-    console.log('test query', workingResult)
+    console.log('test query ->', workingResult)
 
     const { rows: failingResult } = await pool.query('SELECT * FROM notatable')
 
     // this should never appear under any circumstance
-    console.log('failing query', failingResult)
+    console.log('failing query ->', failingResult)
   } catch(error) {
     // this ^ should be the SQL error ("no relation notatable", etc) from the second query
     // but that SQL error is never registered as 'caught'
     // and this error never appears in the console
-    console.error(error)
+    console.error('SQL error from second query ->', error)
   }
 }
 
